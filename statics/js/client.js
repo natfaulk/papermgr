b55790df43b9b52fc879a4ac457a9d0d4de4e345
natfaulk <nfaulk@hotmail.co.uk>
2018-04-18 17:00:27 +1200

    save and remove tags

--- statics/js/client.js
+++ statics/js/client.js
@@ -1,29 +1,53 @@
 const fs = require('fs')
 const path = require('path')
 
+const SAVE_FILE = path.join('savefiles', 'papers.json')
+
 var myapp = angular.module('paperMgr', [])
-myapp.controller('topCtrl', function($scope, $http) {
-  $scope.papers = []
+myapp.controller('topCtrl', ['$scope', '$http', function($s, $http) {
+  $s.papers = []
 
-  $scope.currentPaper = 0
+  $s.currentPaper = 0
 
-  $scope.sidebar = (n) => {
-    $scope.currentPaper = n
+  $s.sidebar = (n) => {
+    $s.currentPaper = n
     let o = document.getElementById('pdfviewer')
     let c = o.cloneNode(true)
-    c.setAttribute('src', $scope.papers[n].path)
+    c.setAttribute('src', $s.papers[n].path)
     o.parentNode.replaceChild(c, o)
   }
 
-  $scope.getSaveFile = () => {
-    fs.readFile(path.join('savefiles', 'papers.json'), 'utf8', function (err, data) {
+  $s.getSaveFile = () => {
+    fs.readFile(SAVE_FILE, 'utf8', function (err, data) {
       if (err) throw err; // we'll not consider error handling for now
-      $scope.papers = JSON.parse(data)
-      console.log($scope.papers)
-      $scope.sidebar($scope.currentPaper)
-      $scope.$apply()
+      $s.papers = JSON.parse(data)
+      console.log($s.papers)
+      $s.sidebar($s.currentPaper)
+      $s.$apply()
     })
   }
 
-  $scope.getSaveFile()
-})
+  $s.addtag = () => {
+    $s.papers[$s.currentPaper].tags.push($s.newTag)
+    $s.newTag = ''
+    $s.saveFileToDisk()
+  }
+
+  $s.removetag = (tag) => {
+    let index = $s.papers[$s.currentPaper].tags.indexOf(tag)
+    if (index !== -1) {
+      $s.papers[$s.currentPaper].tags.splice(index, 1)
+      $s.saveFileToDisk()      
+    }
+  }
+
+  $s.saveFileToDisk = () => {
+    fs.writeFile(SAVE_FILE, JSON.stringify($s.papers), 'utf8', function (err) {
+      if (err) console.log(err);
+  
+      // console.log("The file was saved!");
+    })
+  }
+
+  $s.getSaveFile()
+}])
