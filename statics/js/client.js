6b113f03b7ff00b4218f312704e5ebb7e951af42
natfaulk <nfaulk@hotmail.co.uk>
2018-05-01 12:48:05 +1200

    fixed duplicate tag suggestions

--- statics/js/client.js
+++ statics/js/client.js
@@ -84,7 +84,7 @@ myapp.controller('topCtrl', ['$scope', '$http', function($s, $http) {
 
   $s.addtag = () => {
     $s.papers[$s.currentPaper].tags.push($s.newTag)
-    $s.tagCache.push($s.newTag)    
+    if (!$s.tagCache.includes($s.newTag)) $s.tagCache.push($s.newTag)    
     $s.newTag = ''
     $s.saveFileToDisk()
   }
