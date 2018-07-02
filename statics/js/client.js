natfaulk/papermgr

commit 5fb49e6bc1b1a5a42b13060ff9d8d1d3bfcd4f1d

Fixed where tag not removed from sidebar
When removing all tags, empty tag stays in sidebar

statics/js/client.js
@@ -56,6 +56,7 @@ myapp.controller('topCtrl', ['$scope', '$http', function($s, $http) {
   $('#tags').typeahead({source: $s.tagCache})
 
   $s.regenTagCache = () => {
+    $s.tagCache = []
     $s.papers.forEach((p) => {
       if (p.tags) p.tags.forEach((t) => {
         if (!$s.tagCache.includes(t)) $s.tagCache.push(t)
@@ -95,6 +96,7 @@ myapp.controller('topCtrl', ['$scope', '$http', function($s, $http) {
     if (index !== -1) {
       $s.papers[$s.currentPaper].tags.splice(index, 1)
       $s.saveFileToDisk()
+      $s.regenTagCache()
       $s.$apply() 
     }
   }

commit 8bf2bf6598c389268cd17de2fcd6c0c0e1aaf5bd

fixed broken tag suggestion
Tag suggestion broke with commit 5fb49e6 due to clearing of array breaking reference to it

statics/js/client.js
@@ -56,7 +56,7 @@ myapp.controller('topCtrl', ['$scope', '$http', function($s, $http) {
   $('#tags').typeahead({source: $s.tagCache})
 
   $s.regenTagCache = () => {
-    $s.tagCache = []
+    $s.tagCache.splice(0,$s.tagCache.length)
     $s.papers.forEach((p) => {
       if (p.tags) p.tags.forEach((t) => {
         if (!$s.tagCache.includes(t)) $s.tagCache.push(t)